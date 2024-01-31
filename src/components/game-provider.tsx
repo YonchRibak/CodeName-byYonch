440272b3d371413fb387dc0cec4d1158e72f97c3
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-01-31 05:55:45 +0200

    adjusted game-provider and added type-safety

--- src/components/game-provider.tsx
+++ src/components/game-provider.tsx
@@ -1,10 +1,23 @@
-import { ReactNode, createContext, useContext, useState } from "react";
-
-const GameContext = createContext(null);
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react";
 
 type GameProviderProps = {
   children: ReactNode;
 };
+
+type GameModeController = {
+  gameStarted: boolean;
+  setGameStarted: Dispatch<SetStateAction<boolean>>;
+};
+export const GameContext = createContext<GameModeController | undefined>(
+  undefined
+);
 
 export function GameProvider({ children }: GameProviderProps) {
   const [gameStarted, setGameStarted] = useState(false);
@@ -13,8 +26,4 @@ export function GameProvider({ children }: GameProviderProps) {
       {children}
     </GameContext.Provider>
   );
-}
-
-export function useGameContext() {
-  return useContext(GameContext);
 }
