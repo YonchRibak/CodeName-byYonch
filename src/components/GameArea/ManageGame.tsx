bf4474766bc2f415124cdb1aefce0989428fe6a4
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-03-31 21:13:43 +0300

    implemented socket.io closeRoom to ManageGame component

--- src/components/GameArea/ManageGame.tsx
+++ src/components/GameArea/ManageGame.tsx
@@ -1,39 +1,53 @@
 import { useTranslation } from "react-i18next";
 import { Button } from "../ui/button";
 import useGameContext from "@/Hooks/useGameContext";
+import CaptainsData from "./CaptainsData";
+import { socketService } from "@/Services/SocketService";
 
 function ManageGame(): JSX.Element {
   const { t } = useTranslation();
   const { session, setSession } = useGameContext();
+
+  function handleAbortGame() {
+    setSession((prevSession) => ({
+      ...prevSession,
+      gameStarted: false,
+      turnsPlayed: 0,
+      blueScore: 0,
+      redScore: 0,
+    }));
+
+    socketService.closeRoom(session.sessionId);
+  }
   return (
     <div className="h-full flex flex-col justify-around">
-      <div className="flex flex-col">
-        <span>{t("manageGame.blueScore") + session.blueScore}</span>
-        <span>{t("manageGame.redScore") + session.redScore}</span>
-      </div>
+      {session.captainsConnected < 2 ? ( //If less than 2 captains have connected, render Captains data
+        <CaptainsData />
+      ) : (
+        // if 2 captains have connected, render score and submit button.
+        <div>
+          <div className="flex flex-col">
+            <span>{t("manageGame.blueScore") + session.blueScore}</span>
+            <span>{t("manageGame.redScore") + session.redScore}</span>
+          </div>
+
+          <Button
+            className="text-4xl h-40 bg-primary w-full "
+            onClick={() =>
+              setSession((prevSession) => ({
+                ...prevSession,
+                turnsPlayed: prevSession.turnsPlayed + 1,
+              }))
+            }
+          >
+            {t("manageGame.submitBtn")}
+          </Button>
+        </div>
+      )}
 
       <Button
-        className="text-4xl h-40 bg-primary w-full "
-        onClick={() =>
-          setSession((prevSession) => ({
-            ...prevSession,
-            turnsPlayed: prevSession.turnsPlayed + 1,
-          }))
-        }
-      >
-        {t("manageGame.submitBtn")}
-      </Button>
-      <Button
         className="text-4xl h-40 bg-destructive w-full"
-        onClick={() => {
-          setSession((prevSession) => ({
-            ...prevSession,
-            gameStarted: false,
-            turnsPlayed: 0,
-            blueScore: 0,
-            redScore: 0,
-          }));
-        }}
+        onClick={handleAbortGame}
       >
         {t("manageGame.abortBtn")}
       </Button>
