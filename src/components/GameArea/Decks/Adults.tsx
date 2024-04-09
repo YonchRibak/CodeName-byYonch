482bb73aa73db0822b41e4e707e5b3ddb4963bc3
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-09 18:41:46 +0300

    modified all decks (Family, Adults & Wiki) to be more DRY and created DeckContainer to handle routing of deck

--- src/components/GameArea/Decks/Adults.tsx
+++ src/components/GameArea/Decks/Adults.tsx
@@ -1,64 +1,20 @@
 import useGetWords from "@/Hooks/useGetWords";
-import { useState } from "react";
-import GameCard from "../GameCard";
-import RandomWord from "@/Models/randomWord";
 import "../GameArea.css";
-import useDisplayCards from "@/Hooks/useDisplayCards";
-import useGameContext from "@/Hooks/useGameContext";
 import Loading from "@/components/SharedArea/Interact/Loading";
-import useRevealSelectedCards from "@/Hooks/useRevealSelectedCards";
-function Adults(): JSX.Element {
-  const [showCards, setShowCards] = useState<boolean[]>(Array(25).fill(false));
-  const [currIndexForReplacement, setCurrIndexForReplacement] = useState(0); // the index by which to chose a word from spare words array
-  const [cardStatus, setCardStatus] = useState<string[]>(Array(25).fill(""));
-  const { session, setSession } = useGameContext();
+import CardsContainer from "../CardsContainer";
 
+function Adults(): JSX.Element {
   const { randomWords, isError, isLoading } = useGetWords(false);
 
-  useDisplayCards<RandomWord>(randomWords, setShowCards); // visit 'Hooks/' to learn more what the hook does.
-
-  useRevealSelectedCards(cardStatus, setCardStatus);
-
-  if (isLoading) {
-    return <Loading />; // display loading component.
-  } else if (isError) {
-    return <div>Error!!</div>; // display error component.NOTE TO SELF: create error component.
-  }
+  if (isLoading) return <Loading />; // display loading component.
+  if (isError) return <div>Error!!</div>; // display error component.NOTE TO SELF: create error component.
 
   return (
-    <div className="cards-container">
-      {session.cards?.length &&
-        session.cards.map((card, index) => (
-          <GameCard
-            key={(card as RandomWord).id}
-            index={index}
-            showCard={showCards[index]}
-            wordType="RandomWord"
-            isFamily={false}
-            team={session.teamAscription[index]}
-            cardStatus={cardStatus[index]}
-            setCardStatus={setCardStatus}
-            word={session.cards[index]}
-            onReplaceBtnClick={() => {
-              setSession((prevSession) => {
-                return {
-                  ...prevSession,
-                  cards: [
-                    ...prevSession.cards.slice(0, index),
-                    prevSession.spareCards[
-                      currIndexForReplacement
-                    ] as RandomWord,
-                    ...prevSession.cards.slice(index + 1),
-                  ],
-                };
-              });
-
-              setCurrIndexForReplacement((prev) => prev + 1);
-              // increase currIndexForReplacement so that the next card change will bring a different word from spare words array.
-            }}
-          />
-        ))}
-    </div>
+    <CardsContainer
+      cardsType={"RandomWord"}
+      randomWords={randomWords}
+      isCaptain={false}
+    />
   );
 }
 
--- src/components/GameArea/Decks/Wiki.tsx
+++ src/components/GameArea/Decks/Wiki.tsx
@@ -1,18 +1,18 @@
 import useGetRandomWikiVals from "@/Hooks/useGetRandomWikiVals";
 import i18n from "@/i18n";
-import { useEffect, useState } from "react";
-import GameCard from "../GameCard";
-import WikiObj from "@/Models/WikiObj";
+import { useEffect } from "react";
 import "../GameArea.css";
 import useGameContext from "@/Hooks/useGameContext";
 import Loading from "@/components/SharedArea/Interact/Loading";
-import useRevealSelectedCards from "@/Hooks/useRevealSelectedCards";
+
+import CardsContainer from "../CardsContainer";
+import { useTranslation } from "react-i18next";
+import WikiObj from "@/Models/WikiObj";
 
 function Wiki(): JSX.Element {
-  const [currIndexForReplacement, setCurrIndexForReplacement] = useState(0); // the index by which to chose a word from spare words array
-  const [showCards, setShowCards] = useState<boolean[]>(Array(25).fill(false)); // 25 states defaulted to 'false' for the display of the cards.
-  const [cardStatus, setCardStatus] = useState<string[]>(Array(25).fill(""));
   const { session, setSession } = useGameContext();
+
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (
@@ -22,10 +22,12 @@ function Wiki(): JSX.Element {
     ) {
       setSession((prevSession) => ({
         ...prevSession,
+      
         cards: JSON.parse(localStorage.getItem(`${i18n.language}-initWikis`)),
       }));
       setSession((prevSession) => ({
         ...prevSession,
+      
         spareCards: JSON.parse(
           localStorage.getItem(`${i18n.language}-spareWikis`)
         ),
@@ -33,88 +35,15 @@ function Wiki(): JSX.Element {
     }
   }, [i18n.language]);
 
-  const doneEngFetch = useGetRandomWikiVals("en", 30); // retrieves random values from wikipedia in English, go to 'Hooks/' to learn more.
-  const doneHebFetch = useGetRandomWikiVals("he", 30); // retrieves random values from wikipedia in Hebrew, go to 'Hooks/' to learn more.
+  const { isLoading, isError } = useGetRandomWikiVals(
+    t("configurations.wikiLangName"),
+    30
+  ); // retrieves random values from wikipedia in English, go to 'Hooks/' to learn more.
 
-  useEffect(() => {
-    if (
-      localStorage.getItem(`${i18n.language}-initWikis`) &&
-      localStorage.getItem(`${i18n.language}-spareWikis`)
-    ) {
-      setSession((prevSession) => ({
-        ...prevSession,
-        cards: JSON.parse(localStorage.getItem(`${i18n.language}-initWikis`)),
-      }));
-      setSession((prevSession) => ({
-        ...prevSession,
-        spareCards: JSON.parse(
-          localStorage.getItem(`${i18n.language}-spareWikis`)
-        ),
-      }));
-    }
-  }, [doneEngFetch, doneHebFetch]);
-
-  useEffect(() => {
-    if (
-      (localStorage.getItem("en-US-initWikis") && i18n.language === "en-US") ||
-      (localStorage.getItem("he-IL-initWikis") && i18n.language === "he-IL")
-    ) {
-      const showDelay = 100;
-
-      for (let i = 0; i < 25; i++) {
-        //change state of each showCard props to 'true' on a delay.
-        window.setTimeout(() => {
-          setShowCards((prev) => {
-            const newShowCards = [...prev];
-            newShowCards[i] = true;
-            return newShowCards;
-          });
-        }, i * showDelay);
-      }
-    }
-  }, [doneEngFetch, doneHebFetch]);
-
-  useRevealSelectedCards(cardStatus, setCardStatus);
-  if (
-    (!localStorage.getItem("en-US-initWikis") && i18n.language === "en-US") ||
-    (!localStorage.getItem("he-IL-initWikis") && i18n.language === "he-IL")
-  ) {
-    return <Loading />; // display loading component.
-  }
-
-  return (
-    <div className="cards-container">
-      {session.cards?.length &&
-        session.cards.map((card, index) => (
-          <GameCard
-            key={(card as WikiObj).pageid}
-            index={index}
-            showCard={showCards[index]}
-            wordType="WikiObj"
-            isFamily={false}
-            team={session.teamAscription[index]}
-            cardStatus={cardStatus[index]}
-            setCardStatus={setCardStatus}
-            word={session.cards[index]}
-            onReplaceBtnClick={() => {
-              setSession((prevSession) => {
-                return {
-                  ...prevSession,
-                  cards: [
-                    ...prevSession.cards.slice(0, index),
-                    prevSession.spareCards[currIndexForReplacement] as WikiObj,
-                    ...prevSession.cards.slice(index + 1),
-                  ],
-                };
-              });
-
-              setCurrIndexForReplacement((prev) => prev + 1);
-              // increase currIndexForReplacement so that the next card change will bring a different word from spare words array.
-            }}
-          />
-        ))}
-    </div>
-  );
+  if (isLoading) return <Loading />; // display loading component.
+  if (isError) return <div>Error</div>; // display error component.NOTE TO SELF: create error component.
+  if ((session.cards[0] as WikiObj)?.pageid)
+    return <CardsContainer cardsType="WikiObj" isCaptain={false} />;
 }
 
 export default Wiki;
