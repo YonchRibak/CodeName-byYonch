c3313d7e84e61c1df426d1922a25715de19707a2
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-09 19:31:42 +0300

    installed react-qr-code and implemented it in CaptainsData

--- src/components/GameArea/CaptainsData.tsx
+++ src/components/GameArea/CaptainsData.tsx
@@ -1,13 +1,14 @@
 import useGameContext from "@/Hooks/useGameContext";
 import i18n from "@/i18n";
 import { useTranslation } from "react-i18next";
+import QRCode from "react-qr-code";
 
 function CaptainsData(): JSX.Element {
   const { session } = useGameContext();
   const { t } = useTranslation();
   return (
     <div
-      className={`flex flex-col h-full justify-evenly ${
+      className={`flex flex-col h-full justify-evenly items-center ${
         i18n.language === "en-US" ? "ltr" : "rtl"
       }`}
     >
@@ -16,6 +17,9 @@ function CaptainsData(): JSX.Element {
       </div>
       <div className={`text-4xl font-bold`}>
         {t("captainsData.sessionCode")} {session.sessionId}
+      </div>
+      <div className="bg-white p-5 grid place-items-center w-fit">
+        <QRCode value={`http://localhost:5173/captain/${session.sessionId}`} />
       </div>
     </div>
   );
