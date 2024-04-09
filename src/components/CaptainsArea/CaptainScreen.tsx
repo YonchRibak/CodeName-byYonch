76b857edb81b5bf16f4587653e727ff95953801f
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-09 18:33:07 +0300

    implemented CaptainScreen logic and ui

--- src/components/CaptainsArea/CaptainScreen.tsx
+++ src/components/CaptainsArea/CaptainScreen.tsx
@@ -1,29 +1,57 @@
 import useGameContext from "@/Hooks/useGameContext";
 import { socketService } from "@/Services/SocketService";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import { Session } from "../game-provider";
+import CardsContainer from "../GameArea/CardsContainer";
+import RandomWord from "@/Models/randomWord";
+import WikiObj from "@/Models/WikiObj";
+// import "../GameArea/GameArea.css";
 function CaptainScreen(): JSX.Element {
-  const { session } = useGameContext();
+  const [connected, setConnected] = useState(false);
+  const { session, setSession } = useGameContext();
 
   const params = useParams();
   useEffect(() => {
-    // Connect to the socket server
-    socketService.connect((msg: string) => {
-      console.log(msg);
-    }); // Pass an empty function or null
+    console.log(socketService.isConnected());
+    if (!socketService.isConnected()) {
+      console.log("within", socketService.isConnected());
+      // Connect to the socket server
+      socketService.connect();
 
-    // Emit a connection message to the server indicating a captain is trying to connect
-    socketService.joinRoom(params.gameId);
+      // Emit a connection message to the server indicating a captain is trying to connect
+      socketService.joinRoom(params.gameId, setConnected, setSession);
+    }
+
     // Cleanup function to disconnect from the server when the component unmounts
     return () => {
       socketService.disconnect();
     };
   }, []);
+
+  useEffect(() => {
+    if (socketService.isConnected()) {
+      console.log("useEffect ran");
+      // Receive session data from server:
+      socketService.receiveSessionFromServer((sessionData: Session) => {
+        // setSession(sessionData);
+        // console.log(sessionData);
+        console.log("receiveSession has ran.");
+      });
+    }
+  }, [connected]);
   return (
-    <div className="h-full w-full flex col-span-2 items-center justify-center">
-      <div className="border border-solid border-2">some content</div>
-    </div>
+    <CardsContainer
+      randomWords={
+        (session.cards[0] as WikiObj)?.pageid
+          ? null
+          : (session.cards as RandomWord[])
+      }
+      isCaptain
+      cardsType={
+        (session.cards[0] as WikiObj)?.pageid ? "WikiObj" : "RandomWord"
+      }
+    />
   );
 }
 
