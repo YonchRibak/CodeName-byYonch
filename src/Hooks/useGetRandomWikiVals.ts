8558e232f8cf1c0125456b064fab8feddb32fa39
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-09 19:05:04 +0300

    modified useGetRandomWikiVals hook: 1. storing awaits all queries` isSuccess instead of last query's data (query completion not always by order) 2. block useStoreWikiData if wikis in other language are stored until correct language wikis are fetched 3. return isLoading and isError from useQueries. 4. filter out unwanted wiki values

--- src/Hooks/useGetRandomWikiVals.ts
+++ src/Hooks/useGetRandomWikiVals.ts
@@ -1,14 +1,22 @@
-import i18n from "@/i18n";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useQueries } from "react-query";
 import useStoreWikiData from "./useStoreWikiData";
+import useGameContext from "./useGameContext";
 
 function useGetRandomWikiVals(lang: string, pages: number) {
   const [allData, setAllData] = useState([]); // state to regroup all data to.
   const [isEngStored, setIsEngStored] = useState(true); // boolean to indicate whether there are already items in local storage (rendering the fetch unnecessary)
   const [isHebStored, setIsHebStored] = useState(true); // boolean to indicate whether there are already items in local storage (rendering the fetch unnecessary)
-  const [doneFetch, setDoneFetch] = useState(false); //boolean to indicate whether fetching finished. useQueries's features failed at production.
+  const [blockStoring, setBlockStoring] = useState(true);
+
+  const { session } = useGameContext();
+
+  useEffect(() => {
+    if (!allData.length) {
+      setBlockStoring(false);
+    }
+  }, []);
 
   useEffect(() => {
     // if no corresponding items are in storage, set isStored to false.
@@ -24,7 +32,7 @@ function useGetRandomWikiVals(lang: string, pages: number) {
     ) {
       setIsHebStored(false);
     }
-  }, []);
+  }, [session.cards]);
 
   const url = `https://${lang}.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=extracts&exsentences=1&exintro&explaintext&generator=random&grnnamespace=0&grnlimit=20`;
 
@@ -36,27 +44,45 @@ function useGetRandomWikiVals(lang: string, pages: number) {
           const res = await axios.get(url);
           return res;
         },
-        enabled: i18n.language === "en-US" ? !isEngStored : !isHebStored, // only if isStored is falsy (meaning: no items in storage), run the query.
+        enabled: lang === "en" ? !isEngStored : !isHebStored, // only if isStored is falsy (meaning: no items in storage), run the query.
       };
     })
   );
 
   useEffect(() => {
-    if (results[pages - 1].data?.data) {
-      // if last query resolved,
+    if (results.every((res) => res.isSuccess)) {
+      // if ALL queries have been resolved,
+
       const allWikis = results.reduce((acc, result) => {
         // rejoin all pages
-        return [...acc, ...Object.values(result.data?.data?.query.pages)];
+        return [...acc, ...Object.values(result.data?.data?.query?.pages)];
       }, []);
 
-      setAllData(allWikis);
-      setDoneFetch(true);
+      setAllData(
+        allWikis.filter(
+          (obj) =>
+            obj.extract &&
+            obj.extract !== "האם התכוונתם ל..." &&
+            !obj.extract.includes("may refer to:") &&
+            !obj.extract.includes("is a surname.") &&
+            obj.extract.length > 5
+        )
+      );
+      setBlockStoring(false);
     }
-  }, [results[pages - 1].data?.data]);
+  }, [results.every((res) => res.isSuccess)]);
 
-  useStoreWikiData(allData, lang === "en" ? "en-US" : "he-IL"); // store in local storage.
+  useStoreWikiData(
+    allData,
+    lang === "en" ? "en-US" : "he-IL",
+    blockStoring,
+    setBlockStoring
+  ); // store in local storage and in session (GameContext).
 
-  return doneFetch; // returns a boolean indicting fetching is in progress (false) or has finished (true)
+  const isLoading = results.some((result) => result.isLoading);
+  const isError = results.some((result) => result.isError);
+
+  return { isLoading, isError };
 }
 
 export default useGetRandomWikiVals;
