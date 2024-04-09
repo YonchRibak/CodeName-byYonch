eeb58a8a6b4117fd5c7acfe5b4901b61bba4f54d
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-09 19:09:15 +0300

    updated RandomWord model to new api

--- src/Models/randomWord.ts
+++ src/Models/randomWord.ts
@@ -2,7 +2,7 @@ class RandomWord {
   public id: number;
   public English: string;
   public Hebrew: string;
-  public isFamily: boolean;
+  public isFamily: string;
 }
 
 export default RandomWord;
