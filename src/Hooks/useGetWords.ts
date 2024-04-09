cc0aac915108cd04bd146bcc0220dc09476a9627
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-09 19:06:19 +0300

    modified useGetWords queryKey to be unique

--- src/Hooks/useGetWords.ts
+++ src/Hooks/useGetWords.ts
@@ -2,16 +2,21 @@ import RandomWord from "@/Models/randomWord";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { appConfig } from "@/Utils/appConfig";
+import useGameContext from "./useGameContext";
 
 function useGetWords(isFamily: boolean = false) {
+  const { session } = useGameContext();
   const {
     data: randomWords,
     isLoading,
     isError,
     refetch, // in case there is a need to force useQuery to fetch new data entirely, rather than providing cached data.
   } = useQuery({
-    queryKey: ["getRandomWords"],
+    queryKey: [
+      `getRandomWords-${session.sessionId}-${isFamily ? "family" : "adults"}`,
+    ],
     queryFn: fetchAndHandleData,
+    staleTime: Infinity,
   });
   async function fetchData() {
     const res = await axios.get(appConfig.wordBankUrl);
@@ -23,7 +28,7 @@ function useGetWords(isFamily: boolean = false) {
     if (isFamily) {
       // if isFamily is true, filter through and select only objects that have isFamily: true.
       return fetchedArr
-        .filter((obj: RandomWord) => obj.isFamily)
+        .filter((obj: RandomWord) => obj.isFamily === "True")
         .sort(() => Math.random() - 0.5); // sort the array randomly.
     }
     return fetchedArr.sort(() => Math.random() - 0.5); // if isFamily is false, sort the array randomly,
