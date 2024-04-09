d75daecfe2c811369568dbb32125cafbb16378bd
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-09 18:57:00 +0300

    modified Routing to handle deck selection dynamically using params

--- src/components/LayoutArea/Routing.tsx
+++ src/components/LayoutArea/Routing.tsx
@@ -1,19 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import Wiki from "../GameArea/Decks/Wiki";
-import Family from "../GameArea/Decks/Family";
-import Adults from "../GameArea/Decks/Adults";
 import StartScreen from "./StartScreen";
 import CaptainScreen from "../CaptainsArea/CaptainScreen";
-import CaptainLogin from "../CaptainsArea/CaptainLogin";
+import DeckContainer from "../GameArea/Decks/DeckContainer";
 
 function Routing(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<StartScreen />} />
-      <Route path="/go-nuts" element={<Wiki />} />
-      <Route path="/family" element={<Family />} />
-      <Route path="/adults" element={<Adults />} />
-      <Route path="/captain" element={<CaptainLogin />} />
+      <Route path="/:deck" element={<DeckContainer />} />
       <Route path="/captain/:gameId" element={<CaptainScreen />} />
     </Routes>
   );
