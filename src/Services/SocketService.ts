01cee22e7d8b115906e907ae81a211ff166fdf2b
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-09 19:10:25 +0300

    established socket.io room handling and data delivery

--- src/Services/SocketService.ts
+++ src/Services/SocketService.ts
@@ -1,31 +1,74 @@
 import { appConfig } from "@/Utils/appConfig";
+import { Session } from "@/components/game-provider";
+import { Dispatch, SetStateAction } from "react";
 import { Socket, io } from "socket.io-client";
 
 class SocketService {
   private socket: Socket;
 
-  public connect(callback: Function): void {
+  public connect(): void {
     // Client connects to server:
     this.socket = io(appConfig.baseUrl);
+  }
 
-    // Client listens to server:
-    this.socket.on("message", (msg: string) => {
-      callback(msg);
+  public receiveSessionFromServer(callback: Function): void {
+    // Client listens to sessionData shared by server:
+    this.socket.on("serverSharedSessionData", (sessionData: Session) => {
+      callback(sessionData);
     });
   }
 
-  public initRoom(room: string) {
-    this.socket.emit("initRoom", room);
+  public isConnected(): boolean {
+    return this.socket ? this.socket.connected : false;
   }
+  public initRoom(
+    room: string,
+    callback: Dispatch<SetStateAction<Session>>,
+    session: Session
+  ) {
+    this.socket.emit("initRoom", room);
 
-  public joinRoom(room: string) {
+    // Client listens to room users count
+    this.socket.on("getRoomUsersCount", (count) => {
+      // Update numberOfUsersInRoom in session
+      callback((prevSession: Session) => ({
+        ...prevSession,
+        numberOfUsersInRoom: count,
+      }));
+    });
+
+    // Client listens to other users requesting session data
+    this.socket.on("requestSessionData", (requestingUserId: string) => {
+      console.log("requestSessionData has ran.", requestingUserId);
+      this.provideSessionData(requestingUserId, session);
+    });
+  }
+  public joinRoom(
+    room: string,
+    setConnected: Dispatch<SetStateAction<boolean>>,
+    setSession: Dispatch<SetStateAction<Session>>
+  ): void {
     this.socket.emit("joinRoom", room);
+
+    setConnected(true);
+    // Client listens to sessionData shared by server:
+    this.socket.on("serverSharedSessionData", (sessionData: Session) => {
+      console.log(sessionData);
+      setSession(sessionData);
+    });
   }
 
   public closeRoom(room: string) {
     this.socket.emit("closeRoom", room);
   }
 
+  public provideSessionData(requestingUserId: string, sessionData: Session) {
+    console.log("provideSessionData has ran.", sessionData);
+    this.socket.emit("userProvidesSessionData", {
+      sessionData,
+      targetUser: requestingUserId,
+    });
+  }
   public disconnect(): void {
     // Client disconnects from server:
     this.socket.disconnect();
