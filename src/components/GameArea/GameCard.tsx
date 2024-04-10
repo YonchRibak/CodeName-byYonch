a5b4ed960dea380ae146b95b7e197942b61c26fc
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-10 05:47:09 +0300

    handled indices of cards revealed for captains to see. Also implemented new hook: useManageTextLineBreaks, added bomb icon for captains, and modified style

--- src/components/GameArea/GameCard.tsx
+++ src/components/GameArea/GameCard.tsx
@@ -3,11 +3,13 @@ import i18n from "@/i18n";
 import "./GameArea.css";
 import { Info, RefreshCcw } from "lucide-react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import CardText from "./CardText";
 import useGameContext from "@/Hooks/useGameContext";
 import useKeepScore from "@/Hooks/useKeepScore";
 import useResetCardStatus from "@/Hooks/useResetCardStatus";
+import { GiRollingBomb } from "react-icons/gi";
+import useManageTextLineBreaks from "@/Hooks/useManageTextLineBreaks";
 
 type GameCardProps = {
   index: number;
@@ -24,9 +26,39 @@ type GameCardProps = {
 function GameCard(props: GameCardProps): JSX.Element {
   const [popoverState, setPopoverState] = useState(false);
   const [wordHasBeenReplaced, setWordHasBeenReplaced] = useState(false);
+  const [textValue, setTextValue] = useState("");
+  const { session, setSession } = useGameContext();
 
-  const { session } = useGameContext();
+  useManageTextLineBreaks(
+    // This custom hook manages text line breaks based on the word type and length
+    props.wordType,
+    props.word,
+    setTextValue,
+    wordHasBeenReplaced
+  );
 
+  function handleSelection() {
+    handleCardStatus();
+    pushOrRemoveIndexFromCardsRevealedArr();
+  }
+
+  function pushOrRemoveIndexFromCardsRevealedArr() {
+    if (props.cardStatus === "selected") {
+      // card is being unselected, remove from array:
+      setSession((prev) => ({
+        ...prev,
+        indicesOfRevealedCards: prev.indicesOfRevealedCards.filter(
+          (index) => index !== props.index
+        ),
+      }));
+    } else {
+      // otherwise, card is being selected, add index to array:
+      setSession((prev) => ({
+        ...prev,
+        indicesOfRevealedCards: [...prev.indicesOfRevealedCards, props.index],
+      }));
+    }
+  }
   function handleCardStatus() {
     if (
       session.gameStarted &&
@@ -79,23 +111,32 @@ function GameCard(props: GameCardProps): JSX.Element {
       onClick={
         props.cardStatus === "revealed" || props.isCaptain
           ? null
-          : handleCardStatus
+          : handleSelection
       }
       className={`
     ${/*   general settings:*/ ""} 
-    group relative h-[clamp(${
-      props.isCaptain ? "50px" : "110px"
-    },14vh,15vh)] opacity-0 transform translate-x-5 translate-y-5 transition-all duration-300 ease-in-out
+    group relative h-full opacity-0 transform translate-x-5 translate-y-5 transition-all duration-300 ease-in-out
    
     ${/*card's status. ".selected" is defined in GameArea.css:*/ ""} 
     ${props.cardStatus}
 
+    ${
+      props.isCaptain &&
+      session.indicesOfRevealedCards.includes(props.index) &&
+      "!border-8 !border-white"
+    }
     ${/*in case props.showCard is true, show card:*/ ""} 
     ${props.showCard && "opacity-100 transform translate-x-0 translate-y-0"}
 
     ${
       /* in case isCaptain is true or card is revealed, show team-background:*/ ""
     } 
+
+    ${
+      props.team === "bomb" &&
+      props.isCaptain &&
+      "!border-4 sm:!border-2 border-pink-600"
+    }
     ${
       props.isCaptain || props.cardStatus === "revealed"
         ? assignClassToTeam(props.team)
@@ -117,35 +158,44 @@ function GameCard(props: GameCardProps): JSX.Element {
       >
         <PopoverTrigger
           disabled={props.wordType === "RandomWord"}
-          className={`max-w-min absolute md:top-[1px] md:left-[1px] md:scale-75 top-2 left-2 4xl:top-4 4xl:left-4 4xl:scale-125`}
+          className={`max-w-min absolute sm:top-[-2px] sm:left-[-2px] sm:scale-50 md:scale-[60%] md:top-[1px] md:left-[1px] lg:scale-[150%] lg:top-3 lg:left-4 top-2 left-2 xl:top-4 xl:left-4 xl:scale-125`}
           onMouseEnter={() => setPopoverState(true)}
           onMouseLeave={() => setPopoverState(false)}
         >
-          <Info className={props.wordType === "RandomWord" ? "hidden" : " "} />
+          <Info className={props.wordType === "RandomWord" ? "hidden" : ""} />
         </PopoverTrigger>
         <PopoverContent
           onCloseAutoFocus={(e) => {
             e.preventDefault();
           }}
-          className={"text-xl " + (i18n.language === "en-US" ? "ltr " : "rtl ")}
+          className={i18n.language === "en-US" ? "ltr " : "rtl "}
         >
-          {props.word.extract}
+          <div className="text-xl md:text-3xl sm:text-sm">
+            {props.word.extract}
+          </div>
         </PopoverContent>
       </Popover>
 
-      <CardContent className="card-content h-full flex justify-center items-center p-4 overflow-hidden overflow-ellipsis">
+      <CardContent className="card-content h-full flex justify-center items-center p-4 sm:p-1 overflow-hidden overflow-ellipsis">
         <CardText
-          isCaptain={props.isCaptain}
+          valueLength={
+            props.wordType === "RandomWord" // card content depending on props.wordType.
+              ? i18n.language === "en-US" // card content language depending on current selected language.
+                ? props.word.English.length
+                : props.word.Hebrew.length
+              : props.word.title.length
+          }
           wordHasBeenReplaced={wordHasBeenReplaced}
           className={
             "select-none " + i18n.language === "en-US" ? "ltr " : "rtl "
           }
         >
-          {props.wordType === "RandomWord" // card content depending on props.wordType.
+          {/* {props.wordType === "RandomWord" // card content depending on props.wordType.
             ? i18n.language === "en-US" // card content language depending on current selected language.
               ? props.word.English
               : props.word.Hebrew
-            : props.word.title}
+            : props.word.title} */}
+          {textValue}
         </CardText>
       </CardContent>
 
@@ -157,6 +207,9 @@ function GameCard(props: GameCardProps): JSX.Element {
             props.onReplaceBtnClick();
           }}
         />
+      )}
+      {props.team === "bomb" && props.isCaptain && (
+        <GiRollingBomb className="absolute bottom-3 left-3 scale-[250%] md:scale-[150%] md:bottom-2 md:left-2 xl:scale-[400%] xl:bottom-8 xl:left-8 sm:scale-90 sm:bottom-[2px] sm:left-[2px]" />
       )}
     </Card>
   );
