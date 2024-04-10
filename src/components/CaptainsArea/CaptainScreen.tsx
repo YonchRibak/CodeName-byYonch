6233b2e8a0ca3ba7bf17900827098ef59aee1db0
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-10 05:28:48 +0300

    removed unnecessary code from CaptainScreen

--- src/components/CaptainsArea/CaptainScreen.tsx
+++ src/components/CaptainsArea/CaptainScreen.tsx
@@ -1,13 +1,12 @@
 import useGameContext from "@/Hooks/useGameContext";
 import { socketService } from "@/Services/SocketService";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import CardsContainer from "../GameArea/CardsContainer";
 import RandomWord from "@/Models/randomWord";
 import WikiObj from "@/Models/WikiObj";
 // import "../GameArea/GameArea.css";
 function CaptainScreen(): JSX.Element {
-  const [connected, setConnected] = useState(false);
   const { session, setSession } = useGameContext();
 
   const params = useParams();
@@ -17,7 +16,7 @@ function CaptainScreen(): JSX.Element {
       socketService.connect();
 
       // Emit a connection message to the server indicating a captain is trying to connect
-      socketService.joinRoom(params.gameId, setConnected, setSession);
+      socketService.joinRoom(params.gameId, setSession);
     }
 
     // Cleanup function to disconnect from the server when the component unmounts
@@ -26,12 +25,6 @@ function CaptainScreen(): JSX.Element {
     };
   }, []);
 
-  useEffect(() => {
-    if (socketService.isConnected()) {
-      // Receive session data from server:
-      socketService.receiveSessionFromServer(() => {});
-    }
-  }, [connected]);
   return (
     <CardsContainer
       randomWords={
