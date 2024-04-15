b99e59b021aa34d0a38f1ca720a81608137d23fb
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-15 13:43:09 +0300

    added rules to Routing

--- src/components/LayoutArea/Routing.tsx
+++ src/components/LayoutArea/Routing.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import StartScreen from "./StartScreen";
 import CaptainScreen from "../CaptainsArea/CaptainScreen";
 import DeckContainer from "../GameArea/Decks/DeckContainer";
+import RulesContainer from "../RulesArea/RulesContainer";
 
 function Routing(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<StartScreen />} />
+      <Route path="/rules" element={<RulesContainer />} />
       <Route path="/:deck" element={<DeckContainer />} />
       <Route path="/captain/:gameId" element={<CaptainScreen />} />
     </Routes>
