6205582ce2e2fc58a6115a5646168e6c7011de9e
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-15 13:42:03 +0300

    Family delivers isFamily as prop to cardsContainer

--- src/components/GameArea/Decks/Family.tsx
+++ src/components/GameArea/Decks/Family.tsx
@@ -3,14 +3,20 @@ import "../GameArea.css";
 import Loading from "@/components/SharedArea/Interact/Loading";
 import CardsContainer from "../CardsContainer";
 
-
 function Family(): JSX.Element {
   const { randomWords, isError, isLoading } = useGetWords(true);
 
   if (isLoading) return <Loading />; // display loading component.
   if (isError) return <div>Error</div>; // display error component.NOTE TO SELF: create error component.
 
-  return <CardsContainer cardsType={"RandomWord"} randomWords={randomWords} isCaptain={false} />;
+  return (
+    <CardsContainer
+      cardsType={"RandomWord"}
+      randomWords={randomWords}
+      isCaptain={false}
+      isFamily
+    />
+  );
 }
 
 export default Family;
