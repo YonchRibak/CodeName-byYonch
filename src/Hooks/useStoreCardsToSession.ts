23d3c481997bd2560094adf45ef4ece3251851fd
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-04-15 13:46:35 +0300

    corrected logic in useStoreCardsToSession

--- src/Hooks/useStoreCardsToSession.ts
+++ src/Hooks/useStoreCardsToSession.ts
@@ -1,22 +1,39 @@
 import RandomWord from "@/Models/randomWord";
 import { useEffect } from "react";
 import useGameContext from "./useGameContext";
-import { useParams } from "react-router-dom";
 
 function useStoreCardsToSession(
+  cardsType: string,
   isCaptain: boolean,
-  wordsArr?: RandomWord[]
+  wordsArr?: RandomWord[],
+  isFamily: boolean = false
 ): void {
   const { setSession } = useGameContext();
-  const params = useParams();
 
   useEffect(() => {
-    if (wordsArr?.length && !isCaptain) {
+    if (
+      wordsArr?.length &&
+      !isCaptain &&
+      !sessionStorage.getItem(isFamily ? "family-cards" : "adults-cards")
+    ) {
       setSession((prevSession) => ({
         ...prevSession,
-      
+
         cards: wordsArr.slice(0, 25) as RandomWord[], // set 25 words for initial setting of the game.
         spareCards: wordsArr.slice(25) as RandomWord[], // set other words for spare, in case user opts to replace some of the words.
+      }));
+    } else if (
+      sessionStorage.getItem(isFamily ? "family-cards" : "adults-cards") &&
+      wordsArr?.length &&
+      cardsType === "RandomWord"
+    ) {
+      setSession((prevSession) => ({
+        ...prevSession,
+
+        cards: JSON.parse(
+          sessionStorage.getItem(isFamily ? "family-cards" : "adults-cards")
+        ), // set 25 words for initial setting of the game.
+        spareCards: JSON.parse(sessionStorage.getItem(isFamily ? "family-spares" : "adults-spares"))
       }));
     }
   }, [wordsArr]);
