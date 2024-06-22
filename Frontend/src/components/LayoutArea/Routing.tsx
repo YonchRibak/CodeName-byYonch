54390706905778fc2b34211540378f22a8a73ddd
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-06-22 09:45:13 +0300

    Created About component

--- Frontend/src/components/LayoutArea/Routing.tsx
+++ Frontend/src/components/LayoutArea/Routing.tsx
@@ -3,12 +3,14 @@ import CaptainScreen from "../CaptainsArea/CaptainScreen";
 import DeckContainer from "../GameArea/Decks/DeckContainer";
 import RulesContainer from "../RulesArea/RulesContainer";
 import StartScreen from "./StartScreen";
+import About from "../AboutArea/About";
 
 function Routing(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<StartScreen />} />
       <Route path="/rules" element={<RulesContainer />} />
+      <Route path="/about" element={<About />} />
       <Route path="/:deck" element={<DeckContainer />} />
       <Route path="/captain/:gameId" element={<CaptainScreen />} />
     </Routes>
