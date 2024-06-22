8d57f588e2ba110ef6fb96324854d1f6d83662e6
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-06-22 09:54:37 +0300

    removed unused files

--- Frontend/src/Utils/setNewItemInArrAtIndex.ts
+++
@@ -1,9 +0,0 @@
-import useGameContext from "@/Hooks/useGameContext";
-
-export function setNewItemInArrAtIndex(newItem: any, index: number): void {
-  const { setSession } = useGameContext();
-  setSession((prevSession) => ({
-    ...prevSession,
-    cards: [...prevSession.cards, (prevSession.cards[index] = newItem)],
-  }));
-}
