55ec202fbd94413bd3fa1d97320b599b3ae42ad7
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-06-23 17:42:14 +0300

    created StartScreen

--- Frontend/src/components/LayoutArea/StartScreen.tsx
+++ Frontend/src/components/LayoutArea/StartScreen.tsx
@@ -1,5 +1,16 @@
+import { useTranslation } from "react-i18next";
+import logoSrc from "../../../public/codeNameIcon.png";
 function StartScreen(): JSX.Element {
-  return <div></div>;
+  const { t } = useTranslation();
+  return (
+    <div className="flex flex-col justify-center items-center ">
+      <img src={logoSrc} className="w-[15vw] animate-pulse" />
+      <h1 className="text-7xl font-semibold text-secondary my-12">
+        {t("startScreen.header")}
+      </h1>
+      <p className="text-5xl text-primary">{t("startScreen.description")}</p>
+    </div>
+  );
 }
 
 export default StartScreen;
